07f1e8f6af5bbdec03f696ff5f799637f6fc32cc
memeLordo <simaforest@BAD-BOY>
2024-08-03 21:18:07 +0300

    new .env config

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+from dotenv import dotenv_values
 from telethon import TelegramClient
 
-bot = TelegramClient("bot", 11111, "a1b2c3d4").start(bot_token="TOKEN")
+env_config = dotenv_values(".env")
+
+bot_id = env_config["BOT_ID"]
+bot_hash = env_config["BOT_HASH"]
+bot_token = env_config["BOT_TOKEN"]
+
+bot = TelegramClient("bot", bot_id, bot_hash).start(bot_token=bot_token)
