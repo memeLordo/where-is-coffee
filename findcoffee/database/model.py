6a211c96a061f660046124f821598fba2a0a31ec
memeLordo <simaforest@BAD-BOY>
2024-08-06 02:18:57 +0300

    update model

---
+++ findcoffee/database/model.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..database import Base
+
+primory_key = Annotated[int, mapped_column(primary_key=True)]
+
+
+class UserORM(Base):
+    __tablename__ = "Access Keys"
+
+    id: Mapped[primory_key]
+    telegram_id: Mapped[int]
+    api_id: Mapped[int]
+    api_hash: Mapped[str]
