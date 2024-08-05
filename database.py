2f7033e86d9de67c2744217f6ad23de83425d50e
memeLordo <simaforest@BAD-BOY>
2024-08-05 14:54:03 +0300

    convert_to_db command

--- database.py
+++ database.py
@@ -7,5 +7,12 @@ except FileNotFoundError:
     db.to_csv("keys.csv")
 
 
+def convert_to_form(telegram_id, api_id, api_hash):
+    return pd.DataFrame(
+        {"TELEGRAM_ID": [telegram_id], "API_ID": [
+            api_id], "API_HASH": [api_hash]}
+    )
+
+
 def check_db(id):
     return db.loc[id]
