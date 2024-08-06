72fb47ce3714cdb3ee79c663612b2113286ba34b
memeLordo <simaforest@BAD-BOY>
2024-08-06 03:30:34 +0300

    cleanup database.py

--- findcoffee/database/database.py
+++ findcoffee/database/database.py
@@ -1,13 +1,9 @@
-from typing import Annotated
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-engine = create_engine("sqlite:///./database/sql_catalog.db")
+engine = create_engine(url="sqlite:///sql_catalog.db")
 
 session_factory = sessionmaker(engine)
-
-str_256 = Annotated[str, 256]
 
 
 class Base(DeclarativeBase):
