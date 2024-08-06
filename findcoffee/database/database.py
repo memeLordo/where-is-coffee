671718d661f23c0f61ac1777fcf16a9be5454ce0
memeLordo <simaforest@BAD-BOY>
2024-08-06 04:56:24 +0300

    add __repr__ to Base

--- findcoffee/database/database.py
+++ findcoffee/database/database.py
@@ -9,4 +9,8 @@ session_factory = sessionmaker(engine)
 class Base(DeclarativeBase):
 
     def __repr__(self):
-        pass
+        cols = []
+        for _, col in enumerate(self.__table__.columns.keys()):
+            cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
