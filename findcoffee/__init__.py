15e83d6314a1fe556f9645085d7ae4e71cfaeaa7
memeLordo <simaforest@BAD-BOY>
2024-08-06 04:04:02 +0300

    file logging __main__.py -> __init__.py

--- findcoffee/__init__.py
+++ findcoffee/__init__.py
@@ -1,3 +1,14 @@
+from loguru import logger
+
 from findcoffee import commands, config, database
 
 __all__ = ["commands", "config", "database"]
+
+logger.add(
+    "sessions/bot_session.log",
+    format="{time:DD-MM-YYYY at HH:mm:ss} | {level} | {message}",
+    level="INFO",
+    rotation="2 MB",
+    retention="14 days",
+    compression="zip",
+)
--- findcoffee/__main__.py
+++ findcoffee/__main__.py
@@ -3,15 +3,6 @@ from loguru import logger
 from .config import bot
 from .database.orm import ORM
 
-logger.add(
-    "sessions/bot_session.log",
-    format="{time:DD-MM-YYYY at HH:mm:ss} | {level} | {message}",
-    level="INFO",
-    rotation="2 MB",
-    retention="14 days",
-    compression="zip",
-)
-
 
 @logger.catch
 def main():
