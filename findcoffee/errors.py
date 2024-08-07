93f5e38edc3c291a4e03df251da39831e84c97cd
memeLordo <simaforest@BAD-BOY>
2024-08-07 14:07:09 +0300

    send message after caught TimeoutError

--- findcoffee/errors.py
+++ findcoffee/errors.py
@@ -1,16 +1,32 @@
 
+from loguru import logger
+
+from .config import bot
+
+
 def timeout_handler():
 
     def wrapper(func):
 
         async def wrapped(*args, **kwargs):
 
+            async def send_message(event=args[0], message=" "):
+                async with bot:
+                    await bot.send_message(
+                        event.sender,
+                        message,
+                    )
+
             try:
                 return await func(*args, **kwargs)
             except TimeoutError as e:
                 logger.error(f"Caught {repr(e)}")
                 # print("Got error! ", repr(e))
+                try:
 
+                    await send_message(
+                        message="Ошибка запроса, повторите попытку.",
+                    )
         return wrapped
 
     return wrapper
