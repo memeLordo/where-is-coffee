64001a9dbaaef5f861284ab7090c888893f8ca62
memeLordo <simaforest@BAD-BOY>
2024-08-07 23:12:27 +0300

    cleanup __main__

--- findcoffee/__main__.py
+++ findcoffee/__main__.py
@@ -1,22 +1,13 @@
 from loguru import logger
 
-from .config import bot, bot_token
+from .config import bot
 from .database.orm import ORM
 
 
-@logger.catch
 def start_bot():
     """Start the bot."""
     logger.info("Bot started.")
-    try:
-        bot.run_until_disconnected()
-    except handled_errors as e:
-        logger.error(f"Caught {repr(e)}")
-        bot.start(bot_token=bot_token)
-        bot.send_message(
-            "Ошибка сервера, попробуйте повторить попытку позже.",
-        )
-        bot.run_until_disconnected()
+    bot.run_until_disconnected()
 
 
 if __name__ == "__main__":
