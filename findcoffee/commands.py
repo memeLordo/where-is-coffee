a52a2382b6337bd42e42b70774d9a6f84bc3c161
memeLordo <simaforest@BAD-BOY>
2024-08-07 12:39:08 +0300

    change to event attribute usage

--- findcoffee/commands.py
+++ findcoffee/commands.py
@@ -8,13 +8,12 @@ from .database.orm import ORM
 
 
 async def ask_for_keys(event):
-    async with bot.conversation(event.user_id) as conv:
-
+    async with bot.conversation(event.sender_id) as conv:
         await conv.send_message(Messages.ASK_KEYS[0])
         api_id = await conv.get_response()
         await conv.send_message(Messages.ASK_KEYS[1])
         api_hash = await conv.get_response()
-        ORM.insert_user(event.user_id, api_id, api_hash)
+        ORM.insert_user(event.sender_id, api_id, api_hash)
         # Continue with the conversation
         await conv.send_message("Done!")
     await asyncio.sleep(1)
@@ -23,17 +22,17 @@ async def ask_for_keys(event):
 
 @bot.on(events.NewMessage(pattern="/start"))
 async def start(event):
-    sender = await event.get_sender()
-    logger.info(f"/start initiated by {sender.username}")
+    # sender = await event.get_sender()
+    logger.info(f"/start initiated by {event.sender.username}")
     # ask for keys
     # save keys into .env.client
     # login in client via .env.client
     # from the account start searching for chats
     # return links for chats
-    if ORM.get_user(sender.id) is None:
+    if ORM.get_user(event.sender_id) is None:
         # ORM.insert_user(sender.id, 2, "102")
         logger.info("New user logged in.")
-        logger.debug(f"User id: {sender.id}")
+        logger.debug(f"User id: {event.sender_id}")
         await event.respond(Messages.ASK_LOGIN)
     # ORM.select_users()
     raise events.StopPropagation
