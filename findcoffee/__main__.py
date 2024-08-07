0b4259f268b8041e450d20cfd2d665f91c1355f8
memeLordo <simaforest@BAD-BOY>
2024-08-07 15:24:39 +0300

    handle errors in __main__

--- findcoffee/__main__.py
+++ findcoffee/__main__.py
@@ -1,7 +1,8 @@
 from loguru import logger
 
-from .config import bot
+from .config import bot, bot_token
 from .database.orm import ORM
+from .errors import handled_errors
 
 
 @logger.catch
@@ -9,7 +10,15 @@ def main():
     """Start the bot."""
     ORM.create_tables()
     logger.info("Bot started.")
-    bot.run_until_disconnected()
+    try:
+        bot.run_until_disconnected()
+    except handled_errors as e:
+        logger.error(f"Caught {repr(e)}")
+        bot.start(bot_token=bot_token)
+        bot.send_message(
+            "Ошибка сервера, попробуйте повторить попытку позже.",
+        )
+        bot.run_until_disconnected()
 
 
 if __name__ == "__main__":
